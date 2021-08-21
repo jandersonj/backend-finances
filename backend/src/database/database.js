507547a2df333a4076f163abe09229f637b3f89f
jandersonj <50280581+jandersonj@users.noreply.github.com>
2021-08-21 16:25:39 -0300

    Update database.js

--- backend/src/database/database.js
+++ backend/src/database/database.js
@@ -1,12 +1,10 @@
 const knex = require('knex')({
     client: 'pg',
     version: '13.3',
-    connection: {
-      host : 'ec2-52-0-67-144.compute-1.amazonaws.com',
-      user : 'xqvbkofxrutdbx',
-      password : 'b5b7d0e2ea80bdccd1dc3393ed3357823572acb2f8d220d9b697246d76aa1d9a',
-      database : 'd9n23ltu6a4sa8',
-      ssl : true
+    connection: process.env.DATABASE_URL,
+    pool: {
+        min: 2,
+        max: 10
     }
   });
 
