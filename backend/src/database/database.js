cfa0c9e7bddf81246bd79ea8d9a60a767e2f23c8
jandersonj <50280581+jandersonj@users.noreply.github.com>
2021-08-21 15:54:53 -0300

    Update database.js

--- backend/src/database/database.js
+++ backend/src/database/database.js
@@ -5,7 +5,7 @@ const knex = require('knex')({
       host : 'ec2-52-0-67-144.compute-1.amazonaws.com',
       user : 'xqvbkofxrutdbx',
       password : 'b5b7d0e2ea80bdccd1dc3393ed3357823572acb2f8d220d9b697246d76aa1d9a',
-      database : process.env.DATABASE_URL || 'postgres'
+      database : 'd9n23ltu6a4sa8'
     }
   });
 
