717eea0a81b1627929a71ff044f808698128d10d
jandersonj <50280581+jandersonj@users.noreply.github.com>
2021-08-20 22:31:05 -0300

    Update database.js

--- backend/src/database/database.js
+++ backend/src/database/database.js
@@ -2,9 +2,9 @@ const knex = require('knex')({
     client: 'pg',
     version: '13.2',
     connection: {
-//       host : 'localhost',
-//       user : 'prisma',
-//       password : 'prisma',
+      host : 'ec2-52-0-67-144.compute-1.amazonaws.com',
+      user : 'xqvbkofxrutdbx',
+      password : 'b5b7d0e2ea80bdccd1dc3393ed3357823572acb2f8d220d9b697246d76aa1d9a',
       database : process.env.DATABASE_URL || 'postgres'
     }
   });
